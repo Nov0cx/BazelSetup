006c6609b358ed621f976cc5a80ba90f485551a4
Nov0cx <68678245+Nov0cx@users.noreply.github.com>
2023-10-09 15:33:08 +0200

    Update bazelinit.bat

--- bazelinit.bat
+++ bazelinit.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p prjName=Enter the project name:
 echo workspace(name = "%prjName%") > WORKSPACE.bazel
-echo load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive") > WORKSPACE.bazel
+echo load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive") >> WORKSPACE.bazel
 
 
 set /p bzlVer=Enter the bazel version:
@@ -9,7 +9,7 @@ echo %bzlVer% > .bazelversion
 
 type NUL > .bazelrc
 echo %prjName% > README.md
-echo Using bazel version: %bzlVer% > README.md
-echo Download bazel [here](https://bazel.build/install/bazelisk) > README.md
+echo Using bazel version: %bzlVer% >> README.md
+echo Download bazel [here](https://bazel.build/install/bazelisk) >> README.md
 
 echo Finished setting up Bazel project
